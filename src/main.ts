2973d5cefd1f38a728f527aba6a20624253e730d
Masataka Yonei <masataka.yonei@mescius.com>
2025-06-23 13:26:35 +0900

    「sayHello」関数を追加し、関連するハンドラーとクライアントコードを実装しました

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+import type { Schema } from "../amplify/data/resource"
+import { Amplify } from "aws-amplify"
+import { generateClient } from "aws-amplify/api"
+import outputs from "../amplify_outputs.json"
+
+Amplify.configure(outputs)
+
+const client = generateClient<Schema>()
+
+client.queries.sayHello({
+  name: "Amplify",
+})
